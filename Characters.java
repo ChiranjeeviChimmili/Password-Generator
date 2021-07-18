b189102e9384150a68a201af558253338dfc90af
ChiranjeeviChimmili <akashchimmili@gmail.com>
2021-07-18 16:28:57 -0400

    Add files via upload

---
+++ Characters.java
@@ -0,0 +1,8 @@
+
+public interface Characters {
+
+    String charactersLower = "abcdefghijklmnopqrstuvwxyz";
+    String charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String integers = "1234567890";
+    String specials = ".,/'!@#$%^&*)([]{}~_-";
+}
